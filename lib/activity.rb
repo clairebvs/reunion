5a9b096212502ae2ecde187ff32896aee0c56c84
ClaireBvs <clairembeauvais@gmail.com>
2018-04-05 18:09:50 -0600

    It1 done. But doubt about total cost of an activity

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,21 @@
 class Activity
+  attr_reader :name,
+              :participants,
+              :total_cost
+
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+    @total_cost = 0
+  end
+
+  def add_participants(name, cost)
+    @participants[name] = cost
+  end
+
+  def total_cost
+    @participants.values.sum
+     # binding.pry
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -2,6 +2,44 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
+require 'pry'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_has_name
+    activity = Activity.new("hiking")
+    assert_equal "hiking", activity.name
+  end
+
+  def test_activity_has_participants
+    activity = Activity.new("hiking")
+    assert_equal Hash.new, activity.participants
+  end
+
+  def test_can_add_participants_to_activity
+    activity = Activity.new("hiking")
+    activity.add_participants("Jo", 20)
+    activity.add_participants("John", 10)
+    expected = {"Jo" => 20, "John" => 10}
+    assert_equal expected, activity.participants
+  end
+
+  def test_an_activity_has_total_cost
+    activity = Activity.new("hiking")
+    activity.add_participants("Jo", 20)
+    assert_equal 20, activity.total_cost
+  end
+
+  def test_an_activity_has_total_cost
+    activity = Activity.new("hiking")
+    activity.add_participants("Jo", 20)
+    activity.add_participants("Bill", 40)
+    assert_equal 60, activity.total_cost
+  end
+
+
+  # def test_can_split_the_cost
+  #   activity = Activity.new("hiking")
+  #   assert_equal
+  # end
 end
